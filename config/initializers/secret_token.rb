20625b17a7f75ef4f6d54569447206621cafeb3d
Alex Fogg <me@alexfogg.com>
2013-04-04 10:40:15 -0400

    first commit layout made

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interviewer::Application.config.secret_token = '5928b87026509fe3394a1fa3a6de794ea596d0cd43dfdf6c332601b85a5cf9a34f52bfaaab261b33044e97a9bdf1d6d86c2d0f37c5cacaf0ad2a9a4166407895'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Interviewer::Application.routes.draw do
+  root :to => 'home#index'
+end
