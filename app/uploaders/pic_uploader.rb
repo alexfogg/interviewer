9c58379a060e9e51a0c4c68662475ec52f7b2514
Adi Singh <adityasingh89@gmail.com>
2013-04-04 18:17:09 -0400

    ready to pull

--- app/uploaders/pic_uploader.rb
+++ app/uploaders/pic_uploader.rb
@@ -1,15 +1,23 @@
 class PicUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::RMagick
-  
+
   storage :fog
-  
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
   end
 
   version :thumb do
     process :resize_to_fill => [250, 250]
   end
+
+
+
+
 
 end
