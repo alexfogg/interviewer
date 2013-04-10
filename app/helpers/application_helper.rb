55810e93f1b264767afaa7637a70e9eb2783af73
Stephen Bae <donoage@gmail.com>
2013-04-09 21:52:18 -0400

    purchase improvement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def login_helper
     nav=""
     if @auth.present?
+      nav += "<li>#{link_to('All Interviews', interviews_path)}</li>"
       nav += "<li>#{link_to('Profile', user_path(@auth), :class => 'button tiny success')}</li>"
       nav += "<li>#{link_to(@auth.email, login_path, :method => :delete, :remote => true, :class => 'button tiny alert')}</li>"
 
